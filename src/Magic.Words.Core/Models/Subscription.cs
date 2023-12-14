f3f11172d817d54198beec5e0a13f0eef21a88a1
Serhii Holyboroda <49484600+SerhiiHolyboroda@users.noreply.github.com>
2023-12-14 23:11:39 +0200

    Add 'Subscription' entity and create ApplicationDbContext
    - Added a new 'Subscription' entity to the database schema.
    - Created an 'ApplicationDbContext' to handle interactions with the database.

---
+++ src/Magic.Words.Core/Models/Subscription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magic.Words.Core.Models {
+    public class Subscription {
+        [Key]
+        public int SubscriptionId { get; set; }
+        [Required]
+        public string SubscriptionName { get; set; }
+        public decimal SubscriptionPrice { get; set; }
+
+       
+    }
+}
---
+++ src/Magic.Words.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Magic.Words.Core.Models;
+namespace Magic.Words.Infrastructure.Data {
+    public class ApplicationDbContext : DbContext {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+        }
+
+        public DbSet<Subscription> Subscriptions { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+           
+            modelBuilder.Entity<Subscription>().HasData(
+               new Subscription { SubscriptionId = 1, SubscriptionName = "Standart", SubscriptionPrice = 0 },
+               new Subscription { SubscriptionId = 2, SubscriptionName = "Premium", SubscriptionPrice = 9.99m },
+               new Subscription { SubscriptionId = 3, SubscriptionName = "Royal", SubscriptionPrice = 99.99m }
+               );
+
+        }
+    }
+}
