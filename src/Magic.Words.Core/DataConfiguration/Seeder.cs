5af1ea8b031d61f42a348a426239762d249d3b84
Serhii Holyboroda <49484600+SerhiiHolyboroda@users.noreply.github.com>
2023-12-27 20:04:44 +0200

    Adding DataConfioguration folder with seeder and configuration using IEntityTypeConfiguration

---
+++ src/Magic.Words.Core/DataConfiguration/Seeder.cs
@@ -0,0 +1,19 @@
+﻿using Magic.Words.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magic.Words.Core.DataConfiguration {
+    public class Seeder {
+        public static void Seed(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Subscription>().HasData(
+                new Subscription { SubscriptionId = 1, SubscriptionName = "Standart", SubscriptionPrice = 0 },
+                new Subscription { SubscriptionId = 2, SubscriptionName = "Premium", SubscriptionPrice = 9.99m },
+                new Subscription { SubscriptionId = 3, SubscriptionName = "Royal", SubscriptionPrice = 99.99m }
+                );
+        }
+    }
+}
---
+++ src/Magic.Words.Core/DataConfiguration/SubscriptionConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Magic.Words.Core.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magic.Words.Core.DataConfiguration {
+    public class SubscriptionConfiguration : IEntityTypeConfiguration<Subscription> {
+        public void Configure(EntityTypeBuilder<Subscription> builder) {
+            builder.HasKey(s => s.SubscriptionId);
+            builder.Property(s => s.SubscriptionName).IsRequired();
+            builder.Property(s => s.SubscriptionPrice).IsRequired();
+        }
+    }
+}
